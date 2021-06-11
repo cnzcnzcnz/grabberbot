3c97fbfc5f575103e7629e278be1775fb7b09513
Chandra <cnzrvz@gmail.com>
2021-06-11 14:07:10 +0700

    added imagegrabber, sentences, google suggest added htmlspecialchar for generating html

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+require_once realpath("vendor/autoload.php");
+
+use Buchin\GoogleImageGrabber\GoogleImageGrabber;
+// // use Buchin\SearchTerm\SearchTerm;
+use Buchin\GoogleSuggest\GoogleSuggest;
+use Buchin\SentenceFinder\SentenceFinder;
+// use Buchin\Badwords\Badwords;
+
+// $suggestion = new GoogleSuggest();
+
+$result = GoogleSuggest::grab("speaker");
+$finder = new SentenceFinder;
+$grabber = new GoogleImageGrabber();
+
+
+foreach($result as $keySuggest=>$value){
+    echo $value. "<br>";
+    echo "<br> Keywordnya <b>$value</b> <br>";
+    $sentences = $finder->findSentence($value);
+    foreach($sentences as $keySentences=>$valueSentence){
+        $valueSentence .= " $valueSentence";
+        echo "<p>". $valueSentence ."</p>";
+    }
+    $images = GoogleImageGrabber::grab("$value");
+    foreach($images as $keyImage=>$image){
+        $testSpecialChar = htmlspecialchars("<img src=".$image["url"]." title=".$image["title"]." alt=".$image["alt"].">"."<br><small>".$image["title"]."</small><br>");
+        echo $testSpecialChar;
+    }
+
+
+// }
+
