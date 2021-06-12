d19f7cd8480bd87e0f5fd440cccd2f3c49edd33e
Chandra <cnzrvz@gmail.com>
2021-06-12 15:06:15 +0700

    tambah kondisi <!--more--> ganti keyword

--- index.php
+++ index.php
@@ -9,15 +9,19 @@ use Buchin\SentenceFinder\SentenceFinder;
 
 // $suggestion = new GoogleSuggest();
 
-$result = GoogleSuggest::grab("speaker", "id", "Indonesia", "", "114.7.27.98:8080");
+#"114.7.27.98:8080"
+$result = GoogleSuggest::grab("quotes kehidupan bahasa inggris", "id", "Indonesia", "");
 $finder = new SentenceFinder;
 $grabber = new GoogleImageGrabber();
 
 foreach($result as $keySuggest=>$value){
     $myfile = fopen("$value.html", "w") or die("Unable to open file!");
     fwrite($myfile, "<br><b> $value </b><br>");
-    $images = GoogleImageGrabber::grab("$value");
+    $images = GoogleImageGrabber::grab($value);
     foreach($images as $keyImage=>$image){
+        if($keyImage === 1){
+            $testSpecialChar = htmlspecialchars("<img src=".$image["url"]." title=".$image["title"]." alt=".$image["alt"]."><br>"."<small>".$image["title"]."</small><br><!--more-->");
+        }
         $testSpecialChar = htmlspecialchars("<img src=".$image["url"]." title=".$image["title"]." alt=".$image["alt"]."><br>"."<small>".$image["title"]."</small><br>");
         fwrite($myfile, $testSpecialChar);
     }
