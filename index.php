1a8ce796b2a458c2804b91a26af53b173d6e6991
Chandra <cnzrvz@gmail.com>
2021-06-12 14:47:45 +0700

    remove yang gak penting

--- index.php
+++ index.php
@@ -13,29 +13,13 @@ $result = GoogleSuggest::grab("speaker", "id", "Indonesia", "", "114.7.27.98:808
 $finder = new SentenceFinder;
 $grabber = new GoogleImageGrabber();
 
-// $myfile = fopen("post.txt", "w") or die("Unable to open file!");
-// $txt = "John Doe\n";
-// fwrite($myfile, $txt);
-// $txt = "Jane Doe\n";
-// fwrite($myfile, $txt);
-// fclose($myfile);
-
-
 foreach($result as $keySuggest=>$value){
     $myfile = fopen("$value.html", "w") or die("Unable to open file!");
-    // echo "<br> $value <br>";
-    // $sentences = $finder->findSentence($value);
-    // foreach($sentences as $keySentences=>$valueSentence){
-    //     $valueSentence .= " $valueSentence";
-    //     echo "<p>". $valueSentence ."</p>";
-    // }
     fwrite($myfile, "<br><b> $value </b><br>");
     $images = GoogleImageGrabber::grab("$value");
     foreach($images as $keyImage=>$image){
         $testSpecialChar = htmlspecialchars("<img src=".$image["url"]." title=".$image["title"]." alt=".$image["alt"]."><br>"."<small>".$image["title"]."</small><br>");
         fwrite($myfile, $testSpecialChar);
-        
-        // echo $testSpecialChar;
     }
     fclose($myfile);
 }
